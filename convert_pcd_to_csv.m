cc0ff4f77f31a1ae1bdd1359f1f9e167f5ee7c4f
aniruddha3395 <ani.shembekar@gmail.com>
2019-06-22 16:20:45 -0700

    ply pcd conversions added

---
+++ convert_pcd_to_csv.m
@@ -0,0 +1,15 @@
+function pts = convert_pcd_to_csv(file_name)
+
+pcdata = pcread(file_name);
+pcwrite(pcdata,'temp_file_no_duplicate_caption.ply');
+pcdata = pcread('temp_file_no_duplicate_caption.ply');
+
+delete('temp_file_no_duplicate_caption.ply');
+pts = zeros(pcdata.Count,3);
+
+for i=1:pcdata.Count
+    pts(i,:) = pcdata.Location(i,1:3);
+end
+
+end
+
---
+++ convert_ply_to_csv.m
@@ -0,0 +1,10 @@
+function pts = convert_ply_to_csv(file_name)
+
+pcdata = pcread(file_name);
+pts = zeros(pcdata.Count,3);
+
+for i=1:pcdata.Count
+    pts(i,:) = pcdata.Location(i,1:3);
+end
+
+end
